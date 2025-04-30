3b8274b616f3501c269b1b8a22d628b4bb2f5ef2
Maksym Dzyubenko <ckraer@gmail.com>
2025-04-30 21:49:21 +0300

    Write content of all scripting functions

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+export const addOneContact = async () => {
+    try {
+        const fakeContact = createFakeContact();
+        const contactsList = await readContacts();
+        contactsList.push(fakeContact);
+        await writeContacts(contactsList);
+    }
+    catch(error) {
+        console.log("An error occured: " + error.message);
+    }
+};
 
 addOneContact();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,16 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+export const getAllContacts = async () => {
+    try {
+        const contacts = await readContacts();
+        const readableContactsList = contacts.map(c => {
+            return `${c.name}\r\n\tPhone: ${c.phone}\r\n\tEmail: ${c.email}\r\n\tJob: ${c.job}`;
+        });
+        return readableContactsList.join("\r\n");
+    }
+    catch(error)
+    {
+        return "An error occured: " + error.message;
+    }
+};
 
 console.log(await getAllContacts());
